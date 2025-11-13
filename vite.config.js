c2453af833bf4199a89b1d7faf78b06010a28b6c
an4ek <146551251+an4ek@users.noreply.github.com>
2025-11-13 18:30:46 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   base: '/todo_vue/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // ← твой src
+      '@': path.resolve(__dirname, './src'), 
       'base': path.resolve(__dirname, './src/scss/base'),
       'components': path.resolve(__dirname, './src/scss/components'),
       'layout': path.resolve(__dirname, './src/scss/layout'),
